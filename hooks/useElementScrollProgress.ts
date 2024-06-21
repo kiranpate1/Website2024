ed2b6b68be0a636865e7d87ac17737819473d28d
kiranpate1 <kp8568@gmail.com>
2024-06-21 03:02:55 -0400

    updates

--- hooks/useElementScrollProgress.ts
+++ hooks/useElementScrollProgress.ts
@@ -1,29 +1,29 @@
-import {
-  MotionValue,
-  useMotionValue,
-  useScroll,
-  useTransform,
-} from "framer-motion";
-import { useBounds } from "./useBounds";
-import { MutableRefObject, useEffect, useState } from "react";
+// import {
+//   MotionValue,
+//   useMotionValue,
+//   useScroll,
+//   useTransform,
+// } from "framer-motion";
+// import { useBounds } from "./useBounds";
+// import { MutableRefObject, useEffect, useState } from "react";
 
-/**
- * Tracks the scroll offset relative to the element.
- * Beginning - 0, end - 1.
- * usage: const [containerRef, progress] = useElementScrollProgress(deps);
- *
- * @param deps
- * @returns
- */
-export function useElementScrollProgress<T extends HTMLElement>(
-  deps = [],
-): [MutableRefObject<T>, MotionValue] {
-  const { scrollY } = useScroll();
+// /**
+//  * Tracks the scroll offset relative to the element.
+//  * Beginning - 0, end - 1.
+//  * usage: const [containerRef, progress] = useElementScrollProgress(deps);
+//  *
+//  * @param deps
+//  * @returns
+//  */
+// export function useElementScrollProgress<T extends HTMLElement>(
+//   deps = [],
+// ): [MutableRefObject<T>, MotionValue] {
+//   const { scrollY } = useScroll();
 
-  const [containerRef, bounds] = useBounds<T>(deps);
-  const progress = useTransform(scrollY, (latest) => {
-    return (latest - bounds.top) / bounds.height;
-  });
+//   const [containerRef, bounds] = useBounds<T>(deps);
+//   const progress = useTransform(scrollY, (latest) => {
+//     return (latest - bounds.top) / bounds.height;
+//   });
 
-  return [containerRef, progress];
-}
+//   return [containerRef, progress];
+// }
