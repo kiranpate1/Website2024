56495998862898fe5dfda75a559539180aa3378f
kiranpate1 <kp8568@gmail.com>
2024-06-21 03:06:30 -0400

    f

--- hooks/useRandom.ts
+++
@@ -1,7 +0,0 @@
-import { useEffect, useState } from "react";
-
-export function useRandom(deps: any[] = []) {
-  const [random, setRandom] = useState(0);
-  useEffect(() => setRandom(Math.random()), deps);
-  return random;
-}
